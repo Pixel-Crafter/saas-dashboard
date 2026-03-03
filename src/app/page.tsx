1e8dc78ef6bf88b40d23cf4c085fbda7413d201a
Harshal <88491916+Pixel-Crafter@users.noreply.github.com>
2026-03-03 16:32:39 +0530

    Fix: Seperate login page from root redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,5 @@
-import { ThemeToggle } from "@/components/theme-toggle";
+import { redirect } from "next/navigation";
 
-export default function LoginPage() {
-    return (
-        <main className="min-h-screen flex items-center justify-center relative">
-            <div className="aboslute top-4 right-4">
-                <ThemeToggle />
-            </div>
-
-            <div className="w-[350px] p-6 border rounded-x1 shadow-sm bg-white dark:bg-neutral-900">
-                <h1 className="text-xl font-semibold mb-4">
-                    Login
-                </h1>
-
-                <form className="flex flex-col gap-3">
-                    <input 
-                        type="email" 
-                        placeholder="Email"
-                        className="border p-2 rounded-md bg-transparent"
-                    />
-                    <input 
-                        type="password"
-                        placeholder="Password"
-                        className="border p-2 rounded-md bg-transparent"
-                    />
-                    <button
-                        type="submit"
-                        className="bg-black text-white dark:bg-white dark:text-black py-2 rounded-md"
-                    >
-                        Sign In
-                    </button>
-                </form>
-            </div>
-        </main>
-    )
+export default function Home(){
+    redirect("/login")
 }
